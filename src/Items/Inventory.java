9e2678c7f07dc272c4c0d5954f160babfa0e0232
Nick Roberson <roberson@grinnell.edu>
2016-05-10 11:00:24 -0500

    began building primary classes

---
+++ src/Items/Inventory.java
@@ -0,0 +1,42 @@
+package Items;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory {
+
+	Map<String, Boolean> items;
+
+	public Inventory() {
+		items = new HashMap<>();
+		items.put("keg", false);
+		items.put("person", false);
+		items.put("phone", false);
+		items.put("water", false);
+		items.put("juice", false);
+		items.put("peter michael", false);
+		items.put("dildo", false);
+	}
+
+	public void printItems() {
+		System.out.println("Items in your inventory: ");
+		int i = 1;
+		for (String item : items.keySet()) {
+			if (items.get(item) == true) {
+				System.out.println(i + ". " + item);
+				i++;
+			}
+		}
+	}
+	
+	public void use(String item) {
+		if (items.containsKey(item)) {
+			System.out.println("using " + item);
+		}
+	}
+	
+	public static void main(String[] args){
+		Inventory inv = new Inventory();
+		inv.printItems();
+	}
+}
--- src/TextAdventure.java
+++ src/TextAdventure.java
@@ -1,3 +1,4 @@
+import Environment.Room;
 
 public class TextAdventure {
 
@@ -8,7 +9,22 @@ public class TextAdventure {
 	}
 
 	public static void greet() {
+		// welcome user
 		System.out.println("Hello, welcome our party text adventure.");
 		System.out.println("Get ready for the time of your life.");
+	}
+	
+	public static Room[][] buildEnv() {
+		Room[][] env = new Room[2][4];
+		
+		// build env
+		env[0][0] = new Room("dance");
+		env[0][1] = new Room("bakery");
+		env[1][0] = new Room("game");
+		env[1][1] = new Room("kitchen");
+		env[2][0] = new Room("porch");
+		env[2][1] = new Room("sixth");
+		
+		return env;
 	}
 }
