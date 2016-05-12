e2f8b59f57a3d7bb93bd7fba8f617dadc16ddefc
murphymatt <murphyma@grinnell.edu>
2016-05-11 21:20:43 -0500

    change parser functionality

--- src/UserInput/Parser.java
+++ src/UserInput/Parser.java
@@ -1,33 +1,40 @@
 package UserInput;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import Environment.Room;
+
 public class Parser {
 
-	List<String> commands;
-	
-	public Parser() {
-		commands = new ArrayList<>();
-		commands.add("wait");
-		commands.add("go");
-		commands.add("talk to");
-		commands.add("pick up");
-		commands.add("use");
-		commands.add("attack");
-		commands.add("look at");
-	}
-	public void getInput(){
-		Scanner in = new Scanner(System.in);
-		String input = in.nextLine().toLowerCase();
+    Room currentRoom;
+    Scanner in;
 
-		String action;
-		String auxillary;
-		
-		for (String command : commands) {
-			if (input.contains(command)) {
-				action = command;
-			}
-		}
+    public Parser() {
+	Scanner in = new Scanner(System.in);
+    }
+
+    public void readInput() {
+	String input = in.nextLine().toLowerCase();
+
+	//choose command based on inputted text
+	if (input.contains("wait")) {
+	    currentRoom.waitTurn();
+	} else if (input.contains("go")) {
+	    currentRoom.goTo(input.substring("go".length()).trim());
+	} else if (input.contains("talk to")) {
+	    currentRoom.talkTo(input.substring("talk to".length()).trim());
+	} else if (input.contains("pick up")) {
+	    currentRoom.pickUp(input.substring("pick up".length()).trim());
+	} else if (input.contains("use")) {
+	    currentRoom.pickUp(input.substring("use".length()).trim());
+	} else if (input.contains("attack")) {
+	    currentRoom.attack(input.substring("attack".length()).trim());
+	} else if (input.contains("look at")) {
+	    currentRoom.lookAt(input.substring("look at".length()).trim());
+	} else {
+	    System.out.println("You dont know how to " + input + ".");
 	}
+    }
 }
